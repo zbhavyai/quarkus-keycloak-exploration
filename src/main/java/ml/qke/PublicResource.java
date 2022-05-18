16469401749f5118f900fe6a2acf705de9fe829f
Bhavyai Gupta <zbhavyai@gmail.com>
2022-05-18 10:51:43 -0600

    restoring file to original state

--- src/main/java/ml/qke/PublicResource.java
+++ src/main/java/ml/qke/PublicResource.java
@@ -1,25 +1,13 @@
 package ml.qke;
 
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-
-import org.keycloak.representations.idm.authorization.Permission;
-
-import io.smallrye.mutiny.Uni;
 
 @Path("/api/public")
 public class PublicResource {
 
     @GET
-    public void serve() {
-        // no-op
-    }
-
-    @GET
-    @Path("permissions")
-    public Uni<List<Permission>> get() {
-        return new ProtectedResource().get();
+    public String serve() {
+        return "granted";
     }
 }
